f526e18ff17d98deb9ba1e1d80e8930bb5972c91
fullstackmafia <rugwu@reloadly.com>
2022-01-18 14:17:55 +0100

    made updates

---
+++ server.js
@@ -0,0 +1,66 @@
+require('dotenv').config();
+const express = require("express");
+const app = express();
+const axios = require("axios");
+const stripe = require("stripe")(process.env.STRIPE_KEY);
+const accessToken = process.env.RELOADLY_TOKEN
+
+
+const url = "https://giftcards.reloadly.com/products/120";
+
+const headers = {
+  Accept: "application/com.reloadly.giftcards-v1+json",
+  Authorization: accessToken,
+};
+
+let giftCardsData;
+let giftCardsAmount;
+
+app.get("/giftcards", (req, res) => {
+  axios
+    .get(url, { headers: headers })
+    .then((response) => {
+      giftCardsData = response.data;
+      console.log(giftCardsData)
+      giftCardsAmount = giftCardsData.fixedRecipientDenominations[0] * 100
+      res.send({
+        success: true,
+        data: giftCardsData,
+      });
+    })
+    .catch((error) => {
+      res.send({
+        success: false,
+        data: error,
+      });
+    });
+});
+
+app.post("/create-checkout-session", async (req, res) => {
+  const session = await stripe.checkout.sessions.create({
+    line_items: [
+      {
+        price_data: {
+          currency: giftCardsData.recipientCurrencyCode,
+          product_data: {
+            name: giftCardsData.productName,
+          },
+          unit_amount: giftCardsAmount,
+        },
+        quantity: 1,
+      },
+    ],
+    mode: "payment",
+    success_url: "https://www.reloadly.com/",
+    cancel_url: "https://twitter.com/fullstackmafia",
+  });
+  res.redirect(303, session.url);
+});
+
+app.use(express.static("public"));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "public", "index.html"));
+});
+app.listen(5000, () => {
+  console.log("Server is up at port 5000");
+});
---
+++ src/App.svelte
@@ -0,0 +1,99 @@
+<script>
+import { Stretch } from 'svelte-loading-spinners'
+
+let buju = {
+  productName: undefined,
+  loading: false,
+};
+
+let results;
+
+async function getGiftCards() {
+  buju.loading = true;
+  try {
+    const returnValue = await fetch("/giftcards");
+    const response = await returnValue.json();
+    results = response.data;
+
+    buju = {
+      productName: results.productName,
+      loading: false,
+    };
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+</script>
+
+<main>
+	<div>
+		<p>GET ALL THE CALL OF DUTY GIFT CARDS <br> YOU WANT HERE!</p>
+    	<button class="search_button" on:click={getGiftCards}>SEARCH</button>
+  	</div>
+  	<div>
+    	{#if buju.loading === true}
+	  		<Stretch size="50" color="#FFFFFF" unit="px"/>
+		{:else if buju.productName !== undefined }
+		<div class="results">
+    	  <img class="product_image" src="{results.logoUrls[0]}" alt="gift card images" width="200px"/>
+          	  <p class="redeem_instructions">Redeem Instructions: {results.redeemInstruction.verbose}</p>
+          	  <p class="price">Price: {results.fixedRecipientDenominations[0]} USD</p>
+		  	  <form action="/create-checkout-session" method="POST">
+		  		  <button class="search_button" type="submit">BUY NOW</button>
+	  	  	  </form>
+		</div>
+		{/if}
+	</div>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		margin: 0 auto;
+		font-family: myOutlineFont;
+		font-size: 50px;
+		letter-spacing: 3px;
+		color: rgb(255, 255, 255);
+	}
+
+	.product_image {
+		width: 40%;
+		border-radius: 5px;
+		margin: 20px 0 20px 0;
+	}
+
+	.search_button {
+		background-color: rgba(255, 255, 255, 0.8);
+		font-family: myRegularFont;
+		cursor: pointer;
+		border-radius: 5px;
+		border: none;
+		margin-bottom: 30px;
+	}
+
+	.search_button:hover {
+		background-color:  rgba(179, 179, 179, 0.5);
+		transition: all 0.7s ease;
+	}
+
+	.search_button:focus {
+		background-color:  rgba(179, 179, 179);
+		transition: all 0.1s ease;
+	}
+
+	.search_button:active {
+		background-color: rgba(255, 255, 255, 0.8);
+	}
+
+	.results {
+		cursor: default;
+		background-color: rgba(255, 255, 255, 0.4);
+		border: none;
+		border-radius: 5px;
+		padding: 20px 30px;
+		margin: 30px 50px 30px 30px;
+		font-size: 30px;
+	}
+</style>
